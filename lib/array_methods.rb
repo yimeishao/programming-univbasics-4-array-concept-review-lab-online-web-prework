eaab4aca9e362b870d37480563ec6e9110510a27
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-06 02:46:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,10 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-array.each do 
-result = array[0]
-p result
-    end
+
     
 def find_min_value(array)
   # Add your solution here
