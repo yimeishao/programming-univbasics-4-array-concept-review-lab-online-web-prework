f9b603e28213fb23b55179dab807eca637ebec91
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-06 02:47:21 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,8 +3,10 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-
-    
+array.sort! 
+  return array[-1]
+  end 
+  
 def find_min_value(array)
   # Add your solution here
 end
