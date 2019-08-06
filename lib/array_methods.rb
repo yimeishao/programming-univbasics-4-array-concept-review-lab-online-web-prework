782ce8a9b1ecc8b6eb1b6a40e21c131291168060
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-06 02:30:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
-end
-
+  result = array[0]
+  array.each do |element|
+    if element > result
+      result = element
+    end
+    
 def find_min_value(array)
   # Add your solution here
 end
